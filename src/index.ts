aeb6bfd4c8c4cd5f55fff5a95badaefa9f4e67a2
erenorren <regishashrn07@gmail.com>
2026-02-25 04:53:14 +0000

    update nama

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function hello(nama: string, umur: number): string{
     return 'Selamat Datang '+nama+' di PWL, umur '+umur+'';
 }
 
-const mahasiswa = "Sheren";
+const mahasiswa = "Regisha Sheren";
 const umur:number = 19;
 console.log(hello(mahasiswa, umur));
