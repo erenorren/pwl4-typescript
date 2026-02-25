2c3707331ebb8ef54326a5c623c0ae21b945e071
erenorren <regishashrn07@gmail.com>
2026-02-25 04:46:09 +0000

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function hello(nama: string, umur: number): string{
     return 'Selamat Datang '+nama+' di PWL, umur '+umur+'';
 }
 
-const mahasiswa = "She";
+const mahasiswa = "Sheren";
 const umur:number = 19;
 console.log(hello(mahasiswa, umur));
